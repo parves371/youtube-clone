bb22fc0bacfc3b8c27a630b0f845ca2d609f7f30
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2025-05-04 21:08:03 +0600

    Add SearchPage component with dynamic search functionality

---
+++ src/app/(home)/search/page.tsx
@@ -0,0 +1,22 @@
+export const dynamic = "force-dynamic";
+
+interface PageProps {
+  searchParams: Promise<{
+    query: string | undefined;
+    categoryId: string | undefined;
+  }>;
+}
+const SearchPage = async ({ searchParams }: PageProps) => {
+  const { query, categoryId } = await searchParams;
+  return (
+    <div>
+      Searching for {query} in category {categoryId}
+    </div>
+  );
+};
+
+
+
+
+
+export default SearchPage;
